ee7d8c8741e9e1d021c6fc964e165dcef8c7f0de
Elliot Reymond-Christensen <elliotchristensen@gmail.com>
2018-01-29 17:15:32 -0600

    html structure

---
+++ assets/javascript/app.js
@@ -0,0 +1,6 @@
+//model
+var topics= ["dog", "cat", "ferret", "elephant", "coyote", "wolf", "bird"];
+
+var apiKey= "dc6zaTOxFJmzC";
+
+// sample search http://api.giphy.com/v1/gifs/search?q=messi&api_key=dc6zaTOxFJmzC&limit=10
